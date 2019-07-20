9dcb992801b9cde5dc93e557fe00fba4d78b80f8
matsuihidetoshi <partyrockeverydayimshufflin@gmail.com>
2019-07-20 23:30:21 +0900

    modified for new django project

--- src/components/Evaluation.vue
+++ src/components/Evaluation.vue
@@ -1,14 +1,20 @@
 <template>
   <div class="evaluation">
-    <div>名前: <input style="font-size:18px;margin-bottom:10px;" v-model="name"></div>
+    <div>質問検索: <input style="font-size:18px;margin-bottom:10px;" v-model="name"></div>
     <select v-model="positivity">
       <option disabled value="">Please select one</option>
       <option>1</option>
       <option>2</option>
       <option>3</option>
     </select>
-    <button v-on:click="evaluate()">投票！</button>
-    <div>結果: {{ result }}</div>
+    <button v-on:click="evaluate()">一覧表示</button>
+    <div>
+      <ol>
+        <li v-for="(result, id) in results" v-bind:key="id">
+          {{ result.title }}
+        </li>
+      </ol>
+    </div>
   </div>
 </template>
 <script>
@@ -21,30 +27,20 @@ export default {
   },
   data(){
     return{
-      result: null
+      results: null
     }
   },
   methods: {
     evaluate: function(){
-      var headers = {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
-      }
-      axios({
-        method: 'get',
-        url: 'https://5y46it5qkh.execute-api.ap-northeast-1.amazonaws.com/default/EntityEvaluator',
-        params: {
-          name: this.name,
-          positivity: this.positivity
-        },
-        credentials: true,
-        headers: headers
-      }).then(response => {
-        this.result = response
-      })
+      axios
+      .get('http://52.193.251.60/api/questions/?format=json')
+      .then(response => (this.results = response.data))
     }
   }
 }
 </script>
 <style scoped>
+ul {
+  list-style: none;
+}
 </style>
